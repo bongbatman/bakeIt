bc16e6eac79559febd92d6fde9c22833ae0492cf
bongbatman <nishant05dubey@gmail.com>
2017-08-25 12:16:14 +0530

    add two required activities with fragment

---
+++ app/src/main/java/com/batman/bakeit/ReceipeActivityFragment.java
@@ -0,0 +1,22 @@
+package com.batman.bakeit;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ReceipeActivityFragment extends Fragment {
+
+    public ReceipeActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_receipe, container, false);
+    }
+}
